d7727f85bfd7fc0c88492e864b16480c7f246751
Aman Yadav <58115487+AmanYadav007@users.noreply.github.com>
2021-09-01 09:30:38 +0530

    reintilized

---
+++ script.js
@@ -0,0 +1,44 @@
+// document.write("HEY AMAN LEARNING JAVASCRIPT");
+// console.log("HEY AMAN LOVE TO CODE");
+
+// let a = 10;
+// let b = 20;
+// let add = a + b;
+// console.log(add); 
+
+// let entry = document.getElementById('para');
+// entry.style.color = "#00ff88";
+// entry.style.background = "#000000";
+
+
+// function dataMatch(){
+//     let a = document.getElementById('inp1').value;
+//     let b = document.getElementById('inp2').value;
+//     if(a == b){
+//         alert("THE DATA MATCHED " + a +" "+ b);
+//     }
+//     else if(a != b){
+//         alert("THE DATA DOESN'T MATCHED "+ a +" "+ b);
+//     }
+//     else{
+//         alert("NO INPUT");
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
